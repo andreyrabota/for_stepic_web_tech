d46a6847faa4c4838b5565ec58c184bb4cc77bf7
Andrey_learn <Andreyrabota5@yandex.ru>
2018-07-11 11:08:08 +0300

    hello.py change body

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ def wsgi_application(environ, start_response):
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    body = 'Hello world'
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response = (status, headers)
-    return [body]
+    return body
