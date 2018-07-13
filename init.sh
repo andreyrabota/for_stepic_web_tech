ebb9f4241cf6ee4f6f61e3d4f78d7129950f74ff
Andrey_learn <Andreyrabota5@yandex.ru>
2018-07-13 16:13:55 +0300

    probyem

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/h.py /etc/gunicorn.d/test.py
+gunicorn -c /etc/gunicorn.d/test.py hello:application
