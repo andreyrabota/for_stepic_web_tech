5f5a25b7411ccede11e4493dfb5be854c5f990a9
Andrey_learn <Andreyrabota5@yandex.ru>
2018-07-13 18:14:40 +0300

    stable

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 def application(environ, start_response):
     status = '200 OK'
     headers = [
-        ('Content-Type', 'text/plain')
+        ("Content-Type", "text/plain"),
+		("Content-Length", str(len(data)))
     ]
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
