16e5d69b8ecba27536d85ece4c9f2fd8e193f846
Andrey_learn <Andreyrabota5@yandex.ru>
2018-07-24 12:04:06 +0300

    tryin django_03

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
+sudo ln -s /home/box/web/etc/dj-gun.conf /etc/gunicorn.d/test1
 gunicorn -c /etc/gunicorn.d/test.conf hello:application
 gunicorn service restart
