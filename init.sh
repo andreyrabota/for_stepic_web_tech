4b198220201adfb4a7dc188ffa3c553eac2d4c65
Andrey_learn <Andreyrabota5@yandex.ru>
2018-07-24 17:12:01 +0300

    tryin django_04

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
-sudo ln -s /home/box/web/etc/dj-gun.conf /etc/gunicorn.d/test1
-gunicorn -c /etc/gunicorn.d/test.conf hello:application
-gunicorn service restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
+sudo ln -sf /home/box/web/etc/dj-gun.conf /etc/gunicorn.d/test1
+sudo /etc/init.d/gunicorn restart
